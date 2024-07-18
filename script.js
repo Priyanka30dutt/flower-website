44ddd5b1cdf59f195aabee664fb682112922dacb
Priyanka30dutt <100466514+Priyanka30dutt@users.noreply.github.com>
2024-07-18 18:35:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         console.log('Document clicked!');
+//     });
+// });
